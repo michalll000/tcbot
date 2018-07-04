a55e8e6bae1e44b060fca28eb3bf48bd6adbb0eb
michalll000 <40803091+michalll000@users.noreply.github.com>
2018-07-04 13:01:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 // [SYSTEM/C] Status BOTa
 bot.on("ready", async() => {
-  console.log(`${bot.user.username} startuje z ${bot.users.size} graczami, w ${bot.channels.size} kanałach w ${bot.guilds.size} serwerach`);
+  console.log(`[TC BOT] ${bot.user.username} startuje z ${bot.users.size} graczami, w ${bot.channels.size} kanałach w ${bot.guilds.size} serwerach`);
 
 
     // -Ogląda
@@ -24,15 +24,69 @@ bot.on("ready", async() => {
 // Statystyki
 bot.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
-  console.log(`Dołączono do serwera ${guild.name} (ID: ${guild.id}). Ten serwer posiada ${guild.memberCount} członków i ${guild.channels.size} kanałów! To już ${bot.guilds.size} serwer!`);
+  console.log(`[TC BOT] Dołączono do serwera ${guild.name} (ID: ${guild.id}). Ten serwer posiada ${guild.memberCount} członków i ${guild.channels.size} kanałów! To już ${bot.guilds.size} serwer!`);
   bot.user.setActivity(`tc!cmds | Na ${bot.guilds.size} serwerach!`, {type: "WATCHING"});
 });
 
 bot.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
-  console.log(`Usunięto z serwera ${guild.name} (ID: ${guild.id}). Ten serwer posiada ${guild.memberCount} członków i ${guild.channels.size} kanałów! To był ${bot.guilds.size} serwer!`);
+  console.log(`[TC BOT] Usunięto z któregoś serwera. Teraz TC BOT jest na ${bot.guilds.size} serwerach!`);
   bot.user.setActivity(`tc!cmds | Na ${bot.guilds.size} serwerach!`, {type: "WATCHING"});
 });
+
+// INNE LOGI KONSOLI
+
+bot.on("guildMemberAdd", (member, guild) => {
+  console.log(`[SERWER: ${member.guild.name}] ${member.user.username} dołączył do serwera.` );
+});
+
+bot.on("guildMemberRemove", (member, guild) => {
+  console.log(`[SERWER: ${member.guild.name}] ${member.user.username} wyszedł z serwera.` );
+});
+
+
+
+// LOGI KONSOLI
+bot.on("message", async message => {
+
+// [SYSTEM] Wysyłanie wiadomości
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(); //def 1
+  let sayMessage = args.join(" ");
+
+
+
+ //>>> KOMENDY <<<
+  if(args) {
+    if(message.author.bot)
+      console.log(`[SERWER]: ${message.member.guild} | #${message.channel.name} ([BOT] ${message.author.username}):`, sayMessage);
+    else if (message.author.bot === false)
+      console.log(`[SERWER]: ${message.member.guild} | #${message.channel.name} (${message.author.username}):`, sayMessage);
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -48,12 +102,19 @@ bot.on("message", async message => {
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+  let args = messageArray.slice(1); //def 1
 
 
 
 
 // >>> KOMENDY <<<
+  //if(cmd) {
+    //console.log(`[SERWER]: ${message.member.guild} (${message.author.username}):`, cmd);
+  //};
+
+
+
+
   // PING
   if(cmd === `${prefix}ping`) {
      // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
@@ -70,6 +131,7 @@ bot.on("message", async message => {
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{});
     // And we get the bot to say the thing:
+    //message.channel.send("3Kropek ssie");
     message.channel.send(sayMessage);
 
    }
@@ -78,34 +140,91 @@ bot.on("message", async message => {
 
 
 // Pomoc
-  if(cmd === `${prefix}adminpomoc`){
+  if(cmd === `${prefix}admincmds`){
+    let silikon = bot.user.displayAvatarURL;
     let cmda = new Discord.RichEmbed()
     .setDescription("TC BOT")
-    .setColor("#ff0000")
-    .addField("Zarządzanie serwerem z TC BOT", "Aby móc zarządzać komendami administratora stwórz rolę 'TC BOT ADMIN' i przydziel ją sobie i administracji serwera. Aby poznać komendy dla administratorów wpisz tc!admincmds");
+    .setColor("#8700ff")
+    .setThumbnail(silikon)
+    .addField("Zarządzanie serwerem z TC BOT", "Aby móc zarządzać komendami administratora stwórz rolę 'TC BOT ADMIN' i przydziel ją sobie i administracji serwera.")
+    .addField("tc!admincmds", "Pokazuje to okno z komendami dla administratorów")
+    .addField("tc!wyrzuć <powód (opcjonalnie)>", "Wyrzuca osobę ze serwera")
+    .addField("tc!banuj <powód (opcjonalnie)>", "Wyrzuca i blokuje osobę ze serwera")
+    .addField("tc!purge <liczba (od 2 do 100)>", "Usuwa podaną liczbę wiadomości z kanału");
 
     return message.channel.send(cmda)
   }
+
+// Generowanie zaproszenia
+
+
 
 
 
 // Kick
   if(cmd === `${prefix}wyrzuć`) {
     if(!message.member.roles.some(r=>["TC BOT ADMIN"].includes(r.name)) )
-      return message.reply("Przepraszam! Nie masz uprawnień do używania tej komendy! Wpisz tc!adminhelp");
+      return message.reply("przepraszam! Nie masz uprawnień do używania tej komendy! Wpisz tc!adminpomoc");
+      message.delete().catch(O_o=>{});
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     if(!member)
-      return message.reply("Proszę wskazać prawidłową osobę na tym serwerze!");
+      return message.reply("proszę wskazać prawidłową osobę na tym serwerze!");
+      message.delete().catch(O_o=>{});
     if(!member.kickable)
-      return message.reply("Nie mogę wyrzucić tej osoby! Czyżby ona miała wyższą rolę odemnie? Posiadam uprawnienia administratora?");
+      return message.reply("nie mogę wyrzucić tej osoby! Czyżby ona miała wyższą rolę ode mnie? Posiadam uprawnienia administratora?");
+      message.delete().catch(O_o=>{});
     let reason = args.slice(1).join(' ');
     if(!reason) reason = "[nie podano]";
     await member.kick(reason)
-      .catch(error => message.reply(`Niestety, ${message.author}! Nie mogę wyrzucić tego członka! Błąd : ${error}`));
+      .catch(error => message.reply(`niestety! Nie mogę tego zrobić! Błąd : ${error}`));
+        message.delete().catch(O_o=>{});
     message.reply(`${member.user.tag} został wyrzucony przez ${message.author.tag} z powodu: ${reason}`);
+    message.delete().catch(O_o=>{});
   }
 
 // Ban
+  if(cmd === `${prefix}banuj`) {
+    if(!message.member.roles.some(r=>["TC BOT ADMIN"].includes(r.name)) )
+      return message.reply("przepraszam! Nie masz uprawnień do używania tej komendy! Wpisz tc!adminpomoc");
+      message.delete().catch(O_o=>{});
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("proszę wskazać prawidłową osobę na tym serwerze!");
+      message.delete().catch(O_o=>{});
+    if(!member.bannable)
+      return message.reply("nie mogę zbanować tej osoby! Czyżby ona miała wyższą rolę ode mnie? Posiadam uprawnienia administratora?");
+      message.delete().catch(O_o=>{});
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "[nie podano]";
+    await member.ban(reason)
+      .catch(error => message.reply(`niestety! Nie mogę tego zrobić! Błąd : ${error}`));
+        message.delete().catch(O_o=>{});
+    message.reply(`${member.user.tag} został zbanowany przez ${message.author.tag} z powodu: ${reason}`);
+    message.delete().catch(O_o=>{});
+  }
+
+// Masowe usuwanie wiadomości
+  if(cmd === `${prefix}purge`) {
+    if(!message.member.roles.some(r=>["TC BOT ADMIN"].includes(r.name)) )
+      return message.reply("przepraszam! Nie masz uprawnień do używania tej komendy! Wpisz tc!adminpomoc");
+      message.delete().catch(O_o=>{});
+    const deleteCount = parseInt(args[0], 10);
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("proszę wskazać liczbę od 2 do 100 wiadomości do usunięcia!");
+      message.delete().catch(O_o=>{});
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`niestety! Nie mogę tego zrobić! Błąd : ${error}`));
+      message.delete().catch(O_o=>{});
+    }
+
+
+
+
+
+
+
+
 
 
 
@@ -118,13 +237,14 @@ bot.on("message", async message => {
     .setThumbnail(cmds)
     .addField(">>> Informacje <<<", "Co jakiś czas będą wychodzić nowe komendy!")
     .addField("tc!", "Prefix dla TC BOT")
+    .addField("tc!admincmds", "Komendy administratora")
     .addField("tc!ping", "Mierzy szybkość TC BOT")
-    .addField("tc!powiedz", "Np. 'tc!powiedz coś'. TC BOT usuwa tą wiadomość z komendą i pisze własną z daną treścią")
-    .addField("tc!pomoc admin", "Otwiera Centrum Pomocy wraz z komendami dla administratorów")
-    .addField("tc!elo", "BOT przywita się z tobą elo (tak na testy czy działa lol)")
+    .addField("tc!powiedz <tekst>", "TC BOT usuwa tą wiadomość z komendą i pisze własną z daną treścią")
+    .addField("tc!elo", "BOT przywita się z tobą **elo** (tak na testy czy działa lol)")
     .addField("tc!info", "Informacje na temat TC BOT")
-    .addField("tc!serwer", "Informacje o serwerze jak i o tobie")
-    .addField("tc!profil", "Twój profil TC BOT z informacjami o tobie")
+    .addField("tc!serwer", "Informacje o serwerze")
+    .addField("tc!profil", "Twój profil TC BOT")
+    .addField("tc!zapro", "Tworzy zaproszenie do serwera")
     .addField("tc!despacito", "oofcito")
     .addField("tc!think", "3D EPIK GIF wat u think?")
 
@@ -159,7 +279,7 @@ bot.on("message", async message => {
 
 // Rozmowa - elo
   if(cmd === `${prefix}elo`){
-    return message.channel.send("elo");
+    return message.channel.send("**elo**");
   }
 
 // GIF - think
@@ -174,12 +294,12 @@ bot.on("message", async message => {
     .setDescription("Informacje o TC BOT")
     .setColor("#15f153")
     .setThumbnail(bicon)
-    .addField("Autor", "Custom#9156")
+    .addField("Autor", "*Custom#9156*")
     .addField("Działa od", bot.user.createdAt)
     .addField("Użytkownicy", bot.users.size)
     .addField("Kanały", bot.channels.size)
     .addField("Serwery", bot.guilds.size)
-    .addField("Obecnie na serwerze", message.guild.name)
+    .addField("Obecnie na serwerze", message.guild.name);
 
     return message.channel.send(botembed)
   }
@@ -197,7 +317,8 @@ bot.on("message", async message => {
     .addField("Nazwa serwera", message.guild.name)
     .addField("Stworzony", message.guild.createdAt)
     .addField("Dołączyłeś", message.member.joinedAt)
-    .addField("Liczba członków", message.guild.memberCount);
+    .addField("Liczba członków", message.guild.memberCount)
+    .addField("Kanały", message.guild.channels.size);
 
     return message.channel.send(serverembed);
   }
@@ -211,11 +332,13 @@ bot.on("message", async message => {
     .setThumbnail(picon)
     .addField("Profil użytkownika", message.author.username)
     .addField("Na tym koncie od", message.author.createdAt)
+    .addField("Obecnie na serwerze", message.guild.name)
     .addField("Na serwerze od", message.member.joinedAt)
     .addField("ID:", message.author.id);
 
     return message.channel.send(profilembed);
   }
+
 });
 
 
